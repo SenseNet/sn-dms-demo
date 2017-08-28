c141a61c02b67a08fd56169deb08393f8fbb6678
Aniko Litvanyi <herflis33@gmail.com>
2017-08-28 11:24:22 +0200

    Merge feat/dms-login (#11)
    * Feature/basic pages and layout (#8)
    
    * chore(dependencies): Update dependencies
    
    * chore(package): removed sn-client-cli from dependencies
    
    * feat: Add basic layout and routes
    
    Basic routes are added for login, registration and the dashboard. For choosing between login and
    registration a tabstrip is added. If theres no authenticated user, she will be redirected to the
    login page
    
    * test: Fix test imports
    
    * fix: Add missing dependency
    
    * chore: Fix dependencies
    
    * Merge features/dms login (#10)
    
    * chore(dependencies): Update dependencies
    
    * chore(package): removed sn-client-cli from dependencies
    
    * feat: Add basic layout and routes
    
    Basic routes are added for login, registration and the dashboard. For choosing between login and
    registration a tabstrip is added. If theres no authenticated user, she will be redirected to the
    login page
    
    * test: Fix test imports
    
    * fix: Add missing dependency
    
    * chore: Fix dependencies
    
    * feat(Login): Add login form
    
    * chore: Remove package-lock.json
    
    * chore: Add package-lock.json to the .gitignore
    
    * test: Fix tests

--- src/Sensenet.tsx
+++ src/Sensenet.tsx
@@ -1,22 +1,84 @@
 import * as React from 'react';
 import './Sensenet.css';
+import { styles } from './SensenetStyles'
+import 'typeface-roboto'
+import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
+import { Reducers, Actions } from 'sn-redux'
+import { Dashboard } from './pages/Dashboard'
+import Login from './pages/Login'
+import { Registration } from './pages/Registration'
 
-const logo = require('./assets/logo.png');
+interface ISensenetProps {
+  store,
+  repository,
+  loginState,
+  loginError: string,
+  loginClick: Function,
+  registrationClick: Function
+}
 
-class Sensenet extends React.Component<{}, {}> {
+class Sensenet extends React.Component<ISensenetProps, { isAuthenticated: boolean, params, loginError }> {
+  public name: string = '';
+  public password: string = '';
+
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
+      params: this.props,
+      isAuthenticated: false,
+      loginError: this.props.loginError || ''
+    }
+  }
+
   render() {
     return (
       <div className='Sensenet'>
-        <div className='Sensenet-header'>
-          <img src={logo} className='Sensenet-logo' alt='logo' />
-          <h2>Welcome to sensenet ECM Document Management demo with React</h2>
-        </div>
-        <p className='Sensenet-intro'>
-          Under construction
-        </p>
+        <Route
+          exact
+          path='/'
+          render={routerProps => {
+            const status = this.props.loginState === 1;
+            return status ?
+              <Redirect key='login' to='/login' />
+              : <Dashboard {...routerProps} />;
+          }}
+        />
+        <Route
+          path='/login'
+          render={routerProps => {
+            const status = this.props.loginState === 1;
+            return status ?
+              <Login login={this.props.loginClick} params={{ error: this.props.loginError }} />
+              : <Redirect key='dashboard' to='/' />
+          }}
+        />
+        < Route path='/registration' render={() => <Registration registration={this.props.registrationClick} props={{ name: this.name, password: this.password }} />} />
       </div>
     );
   }
 }
 
-export default Sensenet;
+const mapStateToProps = (state, match) => {
+  return {
+    loginState: Reducers.getAuthenticationStatus(state.sensenet),
+    loginError: Reducers.getAuthenticationError(state.sensenet),
+    store: state
+  }
+}
+
+const userLogin = Actions.UserLogin;
+const userRegistration = () => { };
+
+export default withRouter(connect(
+  mapStateToProps,
+  {
+    loginClick: userLogin,
+    registrationClick: userRegistration
+  })(Sensenet));
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export const Dashboard = () => { 
+    return (
+        <div>Ez itten a dashboard</div>
+    )
+}
