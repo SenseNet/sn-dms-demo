ef06a73191ecaa086a262e0aea1ce1b315474115
Aniko Litvanyi <herflis33@gmail.com>
2017-08-31 13:09:01 +0200

    [KFI]feat(HeaderLogo): Add a white sn logo

--- src/components/AppBarLogo.tsx
+++ src/components/AppBarLogo.tsx
@@ -7,13 +7,25 @@ import {
 const styles = {
     logo: {
         flex: 1,
-        color: '#fff'
+        color: '#fff',
+        width: 40,
+        textDecoration: 'none',
+        fontFamily: 'roboto',
+        marginLeft: 24
+    },
+    logoImg: {
+        maxWidth: 30,
+        maHeight: 30,
+        verticalAlign: 'middle',
+        marginRight: 10
     }
 }
 
+const sensenetLogo = require('../assets/sensenet_white.png')
+
 const AppBarLogo = () => (
-    <Link to='/' style={styles.logo}>
-        <img src='' alt='sensenet' aria-label='sensenet'  />
+    <Link to='/' style={styles.logo} >
+        <img src={sensenetLogo} alt='sensenet' aria-label='sensenet' style={styles.logoImg}  /> sensenet ECM
     </Link>
 )
 
