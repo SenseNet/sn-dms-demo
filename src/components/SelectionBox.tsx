43f7c61568974f7ff7facf2c97a3a909a8f0c89b
Aniko Litvanyi <herflis33@gmail.com>
2017-10-12 12:28:14 +0200

    [KFI]feat(SelectionBox): Add actionmenu to selectionbox

--- src/components/SelectionBox.tsx
+++ src/components/SelectionBox.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { Reducers } from 'sn-redux'
 import { DMSReducers } from '../Reducers'
+import { DMSActions } from '../Actions'
 import Snackbar, { SnackbarContent } from 'material-ui/Snackbar';
 import MoreVert from 'material-ui-icons/MoreVert';
 import IconButton from 'material-ui/IconButton';
@@ -13,19 +14,40 @@ const styles = theme => ({
     },
   });
 
+const batchActions = [
+    {
+        DisplayName : 'Copy selected',
+        Icon: 'copy',
+        Name: 'CopyBatch'
+    },
+    {
+        DisplayName : 'Move selected',
+        Icon: 'move',
+        Name: 'MoveBatch'
+    },
+    {
+        DisplayName : 'Delete selected',
+        Icon: 'delete',
+        Name: 'DeleteBatch'
+    }
+]
+
 interface ISelectionBoxProps {
     selected,
-    classes
+    classes,
+    openActionMenu: Function
 }
 interface ISelectionBoxState {
 }
 
 class SelectionBox extends React.Component<ISelectionBoxProps, ISelectionBoxState> {
     constructor(props) {
-
         super(props)
     }
-    handleClick(e) { }
+    handleClick(e) { 
+        const rect = e.currentTarget.getBoundingClientRect()
+        this.props.openActionMenu(batchActions, this.props.selected, `${this.props.selected.length} Items selected`, { top: rect.top - 150, left: rect.left })
+    }
     render() {
         const { selected, classes } = this.props
         const count = selected.length
@@ -57,4 +79,6 @@ const mapStateToProps = (state, match) => {
     }
 }
 
-export default connect(mapStateToProps, {})(withStyles(styles)(SelectionBox))
+export default connect(mapStateToProps, {
+    openActionMenu: DMSActions.OpenActionMenu
+})(withStyles(styles)(SelectionBox))
