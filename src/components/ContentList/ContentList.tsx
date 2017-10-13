dd1c0d8ae6fc0d7c0d03f532f6b7c87252c646fd
Aniko Litvanyi <herflis33@gmail.com>
2017-10-13 15:21:15 +0200

    [KFI]fix(SelectionMode): Escape selection-mode when deselect the last selected item

--- src/components/ContentList/ContentList.tsx
+++ src/components/ContentList/ContentList.tsx
@@ -90,6 +90,12 @@ class ContentList extends React.Component<ContentListProps, ContentListState> {
                 data: this.props.children
             })
         }
+        if (this.props.selected.length > 0 && !prevOps.selectionModeIsOn) {
+            this.props.selectionModeOn()
+        }
+        else if (this.props.selected.length === 0 && prevOps.selectionModeIsOn) {
+            this.props.selectionModeOff()
+        }
     }
     handleRowSingleClick(e, id, m) {
         const { ids, selected } = this.props;
@@ -118,8 +124,6 @@ class ContentList extends React.Component<ContentListProps, ContentListState> {
                 this.handleSingleSelection(id) :
                 this.handleSimpleSelection(id)
         }
-
-
     }
     handleRowDoubleClick(e, id, type) {
         if (type === 'Folder')
