3ef26ec5e5874d98f8fe2b18ec0c6c7f42ef2daf
Aniko Litvanyi <herflis33@gmail.com>
2018-06-20 12:20:21 +0200

    [KFI]feat: Add actionmenu to the current item in the breadcrumb

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -6,9 +6,12 @@ import MediaQuery from 'react-responsive'
 import {
     withRouter,
 } from 'react-router-dom'
+import * as DMSActions from '../Actions'
 import * as DMSReducers from '../Reducers'
 
+import { Actions, Reducers } from '@sensenet/redux'
 import { icons } from '../assets/icons'
+
 const styles = {
     breadCrumb: {
         flexGrow: 2,
@@ -16,53 +19,103 @@ const styles = {
     breadCrumbItem: {
         textTransform: 'none',
         fontWeight: 'bold',
+        padding: 8,
     },
     breadCrumbIcon: {
-        marginLeft: 30,
+        display: 'inline-block',
+        color: '#777',
+        verticalAlign: 'middle',
+        margin: '0 -7px',
+    },
+    breadCrumbItemLast: {
+        fontWeight: 'bold',
+        fontFamily: 'Raleway ExtraBold',
+    },
+    breadCrumbIconLast: {
+        margin: 0,
     },
     breadCrumbIconLeft: {
-        marginRight: 30,
+    },
+    item: {
+        display: 'inline-block',
+        margin: 0,
+        padding: 0,
     },
 }
 
-class BreadCrumb extends React.Component<{ breadcrumb, history }, {}> {
+interface BreadCrumbProps {
+    actions,
+    history,
+    breadcrumb,
+    openActionMenu,
+    closeActionMenu,
+    currentContent,
+    getActions,
+}
+
+class BreadCrumb extends React.Component<BreadCrumbProps, {}> {
     constructor(props) {
         super(props)
         this.handleClick = this.handleClick.bind(this)
+        this.handleActionMenuClick = this.handleActionMenuClick.bind(this)
     }
     public handleClick(e, id) {
         this.props.history.push(`/${id}`)
     }
     public componentDidUpdate(prevOps) {
-        if (this.props.breadcrumb !== prevOps.breadcrumb) {
+        const { breadcrumb, currentContent, getActions } = this.props
+        if (breadcrumb !== prevOps.breadcrumb) {
             this.setState({
                 data: this.props.breadcrumb,
             })
+            if (!isNaN(currentContent.Id)) {
+                getActions(currentContent.Id, 'ExploreActions')
+            }
         }
     }
+    public handleActionMenuClick(e, content) {
+        const top = e.pageY - e.target.offsetTop
+        const left = e.pageX - e.target.offsetLeft
+        this.props.closeActionMenu()
+        this.props.openActionMenu(this.props.actions, content.Id, content.DisplayName, e.currentTarget, { top: top + 30, left })
+        this.setState({ anchorTop: top, anchorLeft: left })
+    }
     public render() {
+        const { breadcrumb, currentContent } = this.props
         return <div style={styles.breadCrumb}>
             <MediaQuery minDeviceWidth={700}>
                 {(matches) => {
-                    return this.props.breadcrumb.map((n, i) => {
+                    return breadcrumb.map((n, i) => {
                         if (matches) {
-                            return <Button onClick={(event) => this.handleClick(event, n.id)}
-                                key={n.id}
-                                style={styles.breadCrumbItem as any}>
-                                {n.name}
-                                {i !== (this.props.breadcrumb.length - 1) ?
-                                    <Icon style={styles.breadCrumbIcon}>{icons.arrowRight}</Icon> :
+                            return <div style={styles.item} key={i}>
+                                <Button
+                                    aria-owns="actionmenu"
+                                    onClick={
+                                        i !== (breadcrumb.length - 1) ?
+                                            (event) => this.handleClick(event, n.id) :
+                                            (e) => this.handleActionMenuClick(e, currentContent)
+                                    }
+                                    key={n.id}
+                                    style={i === (breadcrumb.length - 1) ? { ...styles.breadCrumbItemLast, ...styles.breadCrumbItem } : styles.breadCrumbItem as any}>
+                                    {n.name}
+                                    {i !== (breadcrumb.length - 1) ?
+                                        '' :
+                                        <Icon style={styles.breadCrumbIconLast} onClick={(e) => this.handleActionMenuClick(e, currentContent)}>{icons.arrowDropDown}</Icon>}
+                                </Button>
+                                {i !== (breadcrumb.length - 1) ?
+                                    <Icon style={styles.breadCrumbIcon}>{icons.arrowRight}</Icon> : ''}
+                            </div>
+                        } else if (!matches && i === (breadcrumb.length - 1)) {
+                            return <div style={styles.item} key={i}>
+                                <Button onClick={(event) => this.handleClick(event, n.id)}
+                                    key={n.id}
+                                    style={styles.breadCrumbItem as any}>
+                                </Button>
+                                {breadcrumb.length > 1 ?
+                                    <Icon style={styles.breadCrumbIconLeft as any}>{icons.arrowLeft}</Icon> :
                                     ''}
-                            </Button>
-                        } else if (!matches && i === (this.props.breadcrumb.length - 1)) {
-                            return <Button onClick={(event) => this.handleClick(event, n.id)}
-                                key={n.id}
-                                style={styles.breadCrumbItem as any}>
-                                {this.props.breadcrumb.length > 1 ?
-                                    <Icon style={styles.breadCrumbIconLeft}>{icons.arrowLeft}</Icon> :
-                                    ''}
                                 {n.name}
-                            </Button>
+                            </div>
                         } else {
                             return null
                         }
@@ -78,7 +131,13 @@ const mapStateToProps = (state, match) => {
     return {
         breadcrumb: DMSReducers.getBreadCrumbArray(state.dms),
         currentId: match.match.params.id,
+        actions: DMSReducers.getActionsOfAContent(state.sensenet.currentcontent),
+        currentContent: Reducers.getCurrentContent(state.sensenet),
     }
 }
 
-export default withRouter(connect(mapStateToProps, {})(BreadCrumb))
+export default withRouter(connect(mapStateToProps, {
+    openActionMenu: DMSActions.openActionMenu,
+    closeActionMenu: DMSActions.closeActionMenu,
+    getActions: Actions.loadContentActions,
+})(BreadCrumb))
