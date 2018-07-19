7ae513ead3ab02e8d1a13c099c56d0d728706b71
Aniko Litvanyi <herflis33@gmail.com>
2018-07-19 11:56:39 +0200

    [KFI]feat: Add custom schema to the repository

--- src/index.tsx
+++ src/index.tsx
@@ -16,15 +16,18 @@ import Sensenet from './Sensenet'
 const history = createHistory()
 import { JwtService } from '@sensenet/authentication-jwt'
 import { sensenetDocumentViewerReducer } from '@sensenet/document-viewer-react'
+import { customSchema } from './assets/schema'
 import './index.css'
 import { MessageBoxHandler } from './utils/MessageBoxHandler'
 import { getViewerSettings } from './ViewerSettings'
 
-const repository = new Repository({
+export const repository = new Repository({
   repositoryUrl: process.env.REACT_APP_SERVICE_URL || 'https://dmsservice.demo.sensenet.com',
   requiredSelect: ['Id', 'Path', 'Name', 'Type', 'ParentId', 'Actions', 'Avatar', 'Owner'] as any,
   defaultExpand: ['Actions', 'Owner'] as any,
+  schemas: customSchema,
 })
+
 const jwt = new JwtService(repository)
 const googleOauthProvider = addGoogleAuth(jwt, { clientId: '188576321252-cad8ho16mf68imajdvai6e2cpl3iv8ss.apps.googleusercontent.com' })
 
