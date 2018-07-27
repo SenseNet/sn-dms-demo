fa1e17ed5983047ac327ffde52afd8aaae53ffa0
Aniko Litvanyi <herflis33@gmail.com>
2018-07-27 12:08:31 +0200

    [KFI]refactor: Refactor messagebar

--- src/components/MessageBar.tsx
+++ src/components/MessageBar.tsx
@@ -1,51 +1,79 @@
-import Fade from '@material-ui/core/Fade'
+import { withStyles } from '@material-ui/core'
 import IconButton from '@material-ui/core/IconButton'
 import Snackbar from '@material-ui/core/Snackbar'
 import CloseIcon from '@material-ui/icons/Close'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { rootStateType } from '..'
 import * as DMSActions from '../Actions'
-import * as DMSReducers from '../Reducers'
 
-// enum MessageMode { error = 'error', warning = 'warning', info = 'info' }
-
-interface MessageBarProps {
-    messagebarProps,
-    OpenMessageBar,
-    CloseMessageBar
+const styles = {
+    window: {
+        left: 14,
+        bottom: 14,
+    },
+    messagebar: {
+        background: '#666666',
+        padding: '0px 14px',
+        fontFamily: 'Raleway Medium',
+        fontSize: 15,
+    },
+    messages: {
+        margin: 0,
+        WebkitPaddingStart: 0,
+    },
+    message: {
+        listStyleType: 'none',
+        padding: 0,
+    },
 }
 
 interface MessageBarState {
     open: boolean
 }
 
-class MessageBar extends React.Component<MessageBarProps, MessageBarState> {
-    public handleClose = (event, reason?) => {
-        this.props.CloseMessageBar()
+const mapStateToProps = (state: rootStateType) => {
+    return {
+        messagebar: state.dms.messagebar,
+    }
+}
+
+const mapDispatchToProps = {
+    openMessageBar: DMSActions.openMessageBar,
+    closeMessageBar: DMSActions.closeMessageBar,
+}
+
+class MessageBar extends React.Component<{ classes } & ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps, MessageBarState> {
+    public closeMessageBar = () => {
+        this.props.closeMessageBar()
     }
     public render() {
-        const { open, content, vertical, horizontal } = this.props.messagebarProps
+        const { classes, messagebar } = this.props
         return (
             <Snackbar
                 anchorOrigin={{
-                    vertical,
-                    horizontal,
+                    vertical: 'bottom',
+                    horizontal: 'left',
                 }}
-                open={open}
-                autoHideDuration={6000}
-                TransitionComponent={Fade}
+                open={messagebar.open}
+                autoHideDuration={messagebar.hideDuration}
+                onClose={this.closeMessageBar}
+                onExited={this.props.messagebar.exited}
+                className={classes.window}
                 ContentProps={{
-                    'aria-describedby': 'message-id',
+                    classes: {
+                        root: classes.messagebar,
+                    },
                 }}
-                message={<span id="message-id">{content.message}</span>}
-                onClose={this.handleClose}
+                message={<ul style={styles.messages}>
+                    {messagebar.content.map((result, index) => <li style={styles.message} key={index}>{result}</li>)}
+                </ul>}
                 action={[
                     <IconButton
                         key="close"
                         aria-label="Close"
                         color="inherit"
-                        // className={classes.close}
-                        onClick={this.handleClose}
+                        onClick={() => this.closeMessageBar()}
                     >
                         <CloseIcon />
                     </IconButton>,
@@ -55,13 +83,4 @@ class MessageBar extends React.Component<MessageBarProps, MessageBarState> {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        messagebarProps: DMSReducers.getMessageBarProps(state.dms),
-    }
-}
-
-export default connect(mapStateToProps, {
-    OpenMessageBar: DMSActions.openMessageBar,
-    CloseMessageBar: DMSActions.closeMessageBar,
-})(MessageBar)
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(MessageBar))
