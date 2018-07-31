27c59c81f565aebc5db14b2457f120f23351a644
Aniko Litvanyi <herflis33@gmail.com>
2018-07-31 10:06:57 +0200

    [KFI]feat: Add basic workspaces list

--- src/components/WorkspaceChooser/WorkspaceList.tsx
+++ src/components/WorkspaceChooser/WorkspaceList.tsx
@@ -1,9 +1,64 @@
+import MenuList from '@material-ui/core/MenuList'
+import { Workspace } from '@sensenet/default-content-types'
 import * as React from 'react'
+import { connect } from 'react-redux'
+import { rootStateType } from '../..'
+import * as DMSActions from '../../Actions'
+import WorkspaceListItem from './WorkspaceListItem'
 
-export class WorkspaceList extends React.Component<{}, {}> {
+const styles = {
+    workspaceList: {
+        padding: 0,
+        margin: 0,
+    },
+}
+
+const mapStateToProps = (state: rootStateType) => {
+    return {
+        workspaces: state.dms.workspaces.all,
+        favorites: state.dms.workspaces.favorites,
+        user: state.sensenet.session.user,
+    }
+}
+
+const mapDispatchToProps = {
+    getWorkspaces: DMSActions.getWorkspaces,
+    getFavorites: DMSActions.loadFavoriteWorkspaces,
+}
+
+interface WorkspaceListState {
+    workspaces: Workspace[],
+    orderedWsList: [],
+}
+
+class WorkspaceList extends React.Component<ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps, WorkspaceListState> {
+    public state = {
+        workspaces: this.props.workspaces,
+        orderedWsList: null,
+        favorites: this.props.favorites,
+    }
+    public static getDerivedStateFromProps(newProps: WorkspaceList['props'], lastState: WorkspaceList['state']) {
+        if (newProps.workspaces.length !== lastState.workspaces.length || lastState.workspaces.length === 0) {
+            newProps.getWorkspaces()
+        }
+        if (lastState.orderedWsList === null || (newProps.favorites.length === 0 && lastState.orderedWsList.length === 0)) {
+            newProps.getFavorites(newProps.user.userName)
+        }
+        return {
+            ...lastState,
+            workspaces: newProps.workspaces,
+            favorites: newProps.favorites,
+            orderedWsList: newProps.workspaces,
+        } as WorkspaceList['state']
+    }
     public render() {
+        const { orderedWsList } = this.state
         return (
-            <div></div>
+            <MenuList style={styles.workspaceList}>
+                {orderedWsList.map((workspace) => <WorkspaceListItem key={workspace.Id} workspace={workspace} />)}
+            </MenuList>
         )
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(WorkspaceList)
