e242b90bba71b5ab3125b77ff5d806f27dcc1636
Aniko Litvanyi <herflis33@gmail.com>
2018-09-18 14:25:10 +0200

    [KFI]fix: Fix filename of the dmstheme

--- src/Sensenet.tsx
+++ src/Sensenet.tsx
@@ -12,7 +12,7 @@ import './Sensenet.css'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import { IOauthProvider } from '@sensenet/authentication-jwt'
 import { rootStateType } from '.'
-import { dmsTheme } from './assets/dmsTheme'
+import { dmsTheme } from './assets/dmstheme'
 import { AuthorizedRoute } from './components/AuthorizedRoute'
 
 const mapStateToProps = (state: rootStateType) => {
