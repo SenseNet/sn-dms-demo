9e59ce815a985536a8e55ce96d18cc2d9e2b2be9
Aniko Litvanyi <herflis33@gmail.com>
2018-09-24 13:41:39 +0200

    [KFI]build: Update webpack config with font and css loading related stuff

--- webpack.config.js
+++ webpack.config.js
@@ -50,14 +50,6 @@ module.exports = {
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
 
-            {
-                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/, /\.eot$/, /\.woff$/, /\.woff2$/, /\.ttf$/],
-                loader: require.resolve('url-loader'),
-                options: {
-                    limit: 10000,
-                    name: 'static/media/[name].[hash:8].[ext]',
-                },
-            },
 
             {
                 test: /\.css$/,
@@ -90,7 +82,16 @@ module.exports = {
                         },
                     },
                 ],
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/, /\.eot$/, /\.woff$/, /\.woff2$/, /\.ttf$/],
+                loader: require.resolve('url-loader'),
+                options: {
+                    limit: 10000,
+                    name: 'static/media/[name].[hash:8].[ext]',
+                },
             }
+
         ]
     },
 
