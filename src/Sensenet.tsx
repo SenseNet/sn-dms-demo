21b2eae36a79e71712f038a55eb0d966be8e3609
herflis <herflis33@gmail.com>
2018-10-30 10:57:04 +0100

    [KFI]feat: Add cookie constent dialog

--- src/Sensenet.tsx
+++ src/Sensenet.tsx
@@ -2,6 +2,7 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import { IOauthProvider } from '@sensenet/authentication-jwt'
 import { LoginState } from '@sensenet/client-core'
 import * as React from 'react'
+import CookieConsent from 'react-cookie-consent'
 import * as Loadable from 'react-loadable'
 import { connect } from 'react-redux'
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom'
@@ -16,6 +17,24 @@ import Login from './pages/Login'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import Registration from './pages/Registration'
 import './Sensenet.css'
+
+const styles = {
+  cookieConsentContainer: {
+    backgroundColor: 'rgb(47, 47, 47)',
+    fontFamily: 'Helvetica, Arial, sans-serif',
+    color: 'rgb(255, 255, 255)',
+    fontSize: '12px',
+    maxWidth: '400px',
+    margin: '20px',
+    borderRadius: '10px',
+  },
+  acceptCookiesButton: {
+    color: '#fff',
+    fontSize: '12px',
+    backgroundColor: 'rgb(72, 165, 84)',
+    cursor: 'pointer',
+  },
+}
 
 const mapStateToProps = (state: rootStateType) => {
   return {
@@ -85,6 +104,14 @@ class Sensenet extends React.Component<SensenetProps & ReturnType<typeof mapStat
           </HashRouter>
         </div>
         <MessageBar />
+        <CookieConsent
+          location="bottom"
+          buttonText="I accept cookies"
+          style={styles.cookieConsentContainer}
+          buttonStyle={styles.acceptCookiesButton}
+        >
+          We use cookies to be able to provide social media features, analyse our traffic and behaviour of the visitors on our website and for marketing purposes. Sometimes we share this anonymised information to 3rd party partner companies. <a href="https://www.eucookie.eu/en/cookieinfo/?url=community.sensenet.com" target="_blank">More information</a>{' '}
+        </CookieConsent>
       </MuiThemeProvider>
     )
   }
