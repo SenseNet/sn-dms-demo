b7dcbadf48aecdec0a8db90c021d7f4b04ef6c23
gallayl <gallay.lajos@gmail.com>
2018-11-05 12:07:46 +0100

    [KFI]feat(QuickSearchInput): added adornments

--- src/components/QuickSearchInput.tsx
+++ src/components/QuickSearchInput.tsx
@@ -1,4 +1,10 @@
+import IconButton from '@material-ui/core/IconButton'
 import Input from '@material-ui/core/Input'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown'
+import ArrowDropUp from '@material-ui/icons/ArrowDropUp'
+import Search from '@material-ui/icons/Search'
+import Tune from '@material-ui/icons/Tune'
 import * as React from 'react'
 import MediaQuery from 'react-responsive'
 
@@ -30,14 +36,32 @@ const styles = {
     },
 }
 
-const quickSearchBox = ({ isOpen, onClick }) => {
+const quickSearchBox = (props: { isOpen: boolean, onClick: () => void }) => {
     return (
         <MediaQuery minDeviceWidth={700}>
             {(matches) => {
                 if (matches) {
-                    return <div>
-                        <Input name="search" style={styles.textStyle} disableUnderline={true} />
-                    </div>
+                    return <Input
+                        name="search"
+                        style={styles.textStyle}
+                        disableUnderline={true}
+                        placeholder="search"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <IconButton disabled>
+                                    <Search color="disabled" />
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton onClick={props.onClick}>
+                                    <Tune />
+                                    {props.isOpen ? <ArrowDropUp /> : <ArrowDropDown />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
                 } else {
                     return <Input name="search" placeholder="search" style={styles.textStyle} disableUnderline={true} />
                 }
