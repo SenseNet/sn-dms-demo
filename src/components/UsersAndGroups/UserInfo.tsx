d11a6a14d727c403b1fa2dfd3913c7a76efe52bc
Aniko Litvanyi <herflis33@gmail.com>
2018-11-07 12:37:31 +0100

    [KFI]feat: Add editing feature for user metadata

--- src/components/UsersAndGroups/UserInfo.tsx
+++ src/components/UsersAndGroups/UserInfo.tsx
@@ -14,6 +14,55 @@ import { resources } from '../../assets/resources'
 // tslint:disable-next-line:no-var-requires
 const defaultAvatar = require('../../assets/no-avatar.jpg')
 
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 12,
+        margin: '10px 0',
+    },
+    leftColumn: {
+        flex: '0 0 0%',
+    },
+    rightColumn: {
+        flex: '1 1 0%',
+        paddingLeft: 10,
+    },
+    avatar: {
+        width: 70,
+        height: 70,
+    },
+    fullName: {
+        color: '#666666',
+        fontFamily: 'Raleway SemiBold',
+        fontSize: 21,
+        flexDirection: 'column',
+    },
+    editIcon: {
+        cursor: 'pointer',
+        fontSize: 18,
+        flexDirection: 'column',
+        marginLeft: 5,
+        marginTop: 5,
+    },
+    email: {
+        color: '#016D9E',
+        textDecoration: 'none',
+        fontFamily: 'Raleway SemiBold',
+        fontSize: 16,
+        marginTop: 5,
+        display: 'block',
+    },
+    phone: {
+        color: '#666666',
+        fontFamily: 'Raleway SemiBold',
+        fontSize: 16,
+        textDecoration: 'none',
+        marginTop: 5,
+        display: 'block',
+    },
+}
+
 // tslint:disable-next-line:no-empty-interface
 interface UserInfoProps {
     user: User,
@@ -35,6 +84,7 @@ const mapDispatchToProps = {
 
 class UserInfo extends React.Component<UserInfoProps & ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps, {}> {
     public handleEditClick = () => {
+        console.log(this.props.user)
         this.props.openDialog(
             <EditPropertiesDialog
                 content={this.props.user}
@@ -46,17 +96,19 @@ class UserInfo extends React.Component<UserInfoProps & ReturnType<typeof mapStat
         // tslint:disable-next-line:no-string-literal
         const avatarUrl = !isLoading && user.Avatar['_deferred'].length > 0 ? repositoryUrl + user.Avatar['_deferred'] : defaultAvatar
         return (
-            isLoading ? null : <Paper style={{ display: 'flex', flexDirection: 'row', padding: 12, margin: '10px 0' }}>
-                <div style={{ flex: '0 0 0%' }}>
-                    <Avatar alt={user.FullName} src={avatarUrl} style={{ width: 70, height: 70 }} />
+            isLoading ? null : <Paper style={styles.container as any}>
+                <div style={styles.leftColumn}>
+                    <Avatar alt={user.FullName} src={avatarUrl} style={styles.avatar} />
                 </div>
-                <div style={{ flex: '1 1 0%' }}>
-                    <Typography>{user.FullName}</Typography>
-                    {user.Name === currentUser ?
-                        <Icon iconName="edit" onClick={() => this.handleEditClick()} />
-                        : null}
-                    <a>{user.Email}</a>
-                    <Typography>{user.Phone}</Typography>
+                <div style={styles.rightColumn}>
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <Typography style={styles.fullName as any}>{user.FullName}</Typography>
+                        {user.Name === currentUser ?
+                            <Icon iconName="edit" onClick={() => this.handleEditClick()} style={styles.editIcon} />
+                            : null}
+                    </div>
+                    <a href={`mailto:${user.Email}`} style={styles.email}>{user.Email}</a>
+                    <Typography><a href={`tel:${user.Phone}`} style={styles.phone}>{user.Phone}</a></Typography>
                 </div>
             </Paper >
         )
